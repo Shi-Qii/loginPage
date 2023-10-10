08c02d75d5d0fd561e7dccafda55c507e18b12c1
shiqihuang <shiqihuang@gashpoint.com>
2023-10-10 14:46:01 +0800

    first commit

---
+++ login_module_gama/src/script.js
@@ -0,0 +1,67 @@
+let isInitialized = false;
+let userCallbacks = {
+    onLoginSuccess: null,
+    onLoginFailure: null
+};
+
+let loginTemplate = `
+<div class="login-container">
+  <input type="text" id="username" placeholder="Username" />
+  <input type="password" id="password" placeholder="Password" />
+  <button id="login-btn">Login</button>
+</div>
+`;
+
+function renderLogin(containerId, callbacks) {
+    const container = document.getElementById(containerId);
+    if (container) {
+        container.innerHTML = loginTemplate;
+    }
+
+    if (callbacks) {
+        if (callbacks.onLoginSuccess) {
+            userCallbacks.onLoginSuccess = callbacks.onLoginSuccess;
+        }
+
+        if (callbacks.onLoginFailure) {
+            userCallbacks.onLoginFailure = callbacks.onLoginFailure;
+        }
+    }
+}
+
+function init(params) {
+    if (!params) {
+        console.error('Initialization parameters are required.');
+        return;
+    }
+
+    // 初始化參數設定
+
+    isInitialized = true;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginBtn = document.getElementById('login-btn');
+    if (loginBtn) {
+        loginBtn.addEventListener('click', function() {
+            if (!isInitialized) {
+                console.error('The login module has not been initialized.');
+                return;
+            }
+
+            // 登入功能
+            let loginSuccessful = true;  // 假定為登入成功
+
+            if (loginSuccessful) {
+                if (userCallbacks.onLoginSuccess) {
+                    userCallbacks.onLoginSuccess();
+                }
+            } else {
+                if (userCallbacks.onLoginFailure) {
+                    userCallbacks.onLoginFailure();
+                }
+            }
+        });
+    }
+});
+
